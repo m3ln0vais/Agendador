0eb3a2502a2e6d12c3474c4077c4fc8b5c0007a3
mel <mellnovais14@gmail.com>
2023-04-11 18:00:40 -0300

    informações da conta

--- src/main/java/com/rc20/agendador/controllers/ClientAppController.java
+++ src/main/java/com/rc20/agendador/controllers/ClientAppController.java
@@ -1,9 +1,15 @@
 package com.rc20.agendador.controllers;
 
+import com.rc20.agendador.models.Client;
 import com.rc20.agendador.services.ClientService;
+import jakarta.validation.Valid;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -23,8 +29,21 @@ public class ClientAppController {
     }
     
     @GetMapping("/clientForm")
-    public String form(){
+    public String form(Model model, Optional<Long> id) {
+        model.addAttribute("client", 
+                id.isPresent() ? clientService.findById(id.get()).get() : new Client());
         return "views/clientApp/clientForm";
     }
     
+    @PostMapping("/clientForm")
+    public String register(Model model, @Valid Client client, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {;
+            model.addAttribute("errors", bindingResult.getAllErrors());
+            model.addAttribute("client", client);
+            return "views/clientApp/clientForm";
+        }
+        
+        clientService.save(client);
+        return "redirect:/mainClient?id=" + client.getId();
+    }
 }
